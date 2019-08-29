c1823f21d832fb231924480007a1567470778bdd
CHoran7 <52431993+CHoran7@users.noreply.github.com>
2019-08-28 19:32:28 -0600

    Changed time format and have correct file path

--- drillStep100.py
+++ drillStep100.py
@@ -1,10 +1,9 @@
 import os
+import time
 
 fPath = 'C:\\Drill File\\'
 
 fList = os.listdir(fPath)
 for i in fList:
     if i.endswith('.txt'):
-        print(i + " was last modified " + str(os.path.getmtime(fPath)) + " seconds ago.")
-        
-
+        print(i + " was last modified " + time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(os.path.getmtime(os.path.join(fPath, i)))))
